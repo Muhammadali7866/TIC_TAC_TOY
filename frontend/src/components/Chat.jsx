651197c8fa9b7ce21269bc91bf95e20cb131f5de
Muhammadali7866 <malikali8157@gmail.com>
2024-07-10 19:19:08 +0500

    in flow of chat page

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,36 +1,28 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import { faRobot, faUser, faBars } from "@fortawesome/free-solid-svg-icons"; // Import the specific icon
+import {
+  faRobot,
+  faUser,
+  faBars,
+  faPaperPlane,
+} from "@fortawesome/free-solid-svg-icons"; // Import the specific icon
 
 function Chat() {
   return (
     <>
-      <div className="bg-custom-dark h-screen">
-        <div className="flex items-center justify-center">
-          <div className=" w-[669px] h-[500px] rounded-lg mt-5 flex flex-row">
-            <div className="left flex w-[200px] h-[500px] rounded-l-lg bg-[#1f2d46] border-r-1 border-[#2e3b55]">
-              <div className="flex flex-row w-[199px] h-[33px] mt-2 ml-1">
-                <div className="w-[23px] h-[23px]">
-                  <FontAwesomeIcon
-                    icon={faBars}
-                    style={{ height: "17px", width: "17px", color: "white" }}
-                    className="opacity-20"
-                  />
-                </div>
-                <div className="w-[157px] h-[22px]">
-                  <input
-                    className=" text-black rounded-2xl w-[170px] bg-[#1f2d46] border border-[#2e3b55] pl-2 text-white"
-                    placeholder="Search"
-                  />{" "}
-                </div>
-              </div>
+      <div className="bg-custom-dark flex justify-center items-center min-h-screen">
+        <div className="w-[900px] h-[600px]  flex justify-around items-center rounded-lg border-20 bg-[#142534]">
+          <div className="rounded-lg w-[250px] h-[585px] bg-[#172a3a]">
+            <div className="w-[180px] h-[37px] rounded-md mt-2 ml-3 px-1 py-1">
+              <input className="bg-[#172a3a]" type="text" placeholder="search"/>
             </div>
-            <div className="right w-[369px] h-[500px] bg-[#101f36] rounded-r-lg">
-              <div className="flex flex-col justify-center">
-                <div className="w-[369px] h-[34px] bg-[#212e45] rounded-tr-lg"></div>
-                <div className="w-[369px] h-[34px] bg-[#212e45] rounded-tr-lg"></div>
-              </div>
+          </div>
+          <div className="rounded-lg w-[600px] h-[585px] flex flex-col justify-between">
+            <div className="w-[600px] h-[55px] rounded-lg bg-[#172a3a]"> 
+              <div></div>
+              <div>User Name</div>
             </div>
+            <div className="w-[550px] h-[55px] rounded-lg bg-[#172a3a]"></div>
           </div>
         </div>
       </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        'custom-dark': 'rgb(15, 23, 42)',
-        "custom-light-dark":"#1f3540"
+        "custom-dark": "rgb(15, 23, 42)",
+        "custom-light-dark": "#1f3542",
+        "custom-ligt1": "rgb(15, 27, 44)",
       },
     },
   },
   plugins: [],
-}
+};
